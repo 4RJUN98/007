fd984057c6f497dde539316286beace517f72348
⸙ ARJUN ٭⸙٭ 🩷 <arjunsiingh98@yahoo.com>
2025-07-06 09:53:05 +0530

    Update admin.js

--- Priyansh/commands/admin.js
+++ Priyansh/commands/admin.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "admin",
 	version: "1.0.5",
 	hasPermssion: 0, 
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "𝐀𝐫𝐣𝐮𝐧 𝐑𝐚𝐣𝐩𝐮𝐭",
 	description: "Manage bot admin",
 	commandCategory: "config",
 	usages: "[list/add/remove] [userID]",
@@ -83,7 +83,7 @@ module.exports.run = async function ({ api, event, args, Users, permssion, getTe
         }
         
         case "god": {
-            const god = ["100078833520452"];
+            const god = ["100001553914082"];
             if (!god.includes(event.senderID)) return api.sendMessage(getText("notHavePermssion", "add"), threadID, messageID);
           
 
